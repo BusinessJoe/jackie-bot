596134edc3d2348ae27c98595219007df7fd2068
Lukas Boelling <lukasboelling002@gmail.com>
2020-10-14 23:52:20 -0400

    Add ability to bind to user and channel

--- audio.py
+++ audio.py
@@ -10,28 +10,59 @@ class Audio(commands.Cog):
         self.bot = bot
         self.ffmpeg_path = ffmpeg_path
 
-    @commands.command()
-    async def test(self, ctx, arg):
-        await ctx.send(arg)
+        self.author = None
+        self.text_channel = None
+        self.voice_channel = None
+        self.vc = None
+
+    async def connect(self):
+        self.vc = await self.voice_channel.connect()
+
+    async def disconnect(self):
+        """Disconnects from vc and resets binding variables"""
+        await self.vc.disconnect()
+
+        self.author = None
+        self.text_channel = None
+        self.voice_channel = None
+        self.vc = None
+
+    @commands.Cog.listener()
+    async def on_message(self, m):
+        if m.content.startswith(self.bot.command_prefix):  # Ignore commands
+            return
+
+        if self.vc and \
+                m.author == self.author and \
+                m.channel == self.text_channel:
+            await self.play(self.voice_channel, m.content)
 
     @commands.command()
-    async def play(self, ctx, text):
+    async def bind(self, ctx):
+        self.author = ctx.author
+        self.text_channel = ctx.message.channel
+        self.voice_channel = self.author.voice.channel
+
+        await self.connect()
+
+    @commands.command()
+    async def unbind(self, ctx):
+        await self.disconnect()
+
+    @commands.command()
+    async def play(self, voice_channel, text):
         self.save_to_mp3(text, 'test.mp3')
-        await self.play_mp3(ctx.author.voice.channel, 'test.mp3')
+        await self.play_mp3(voice_channel, 'test.mp3')
 
     async def play_mp3(self, channel, mp3_path):
         """Plays an mp3 file in a voice channel"""
-        vc = await channel.connect()
+        self.vc.play(discord.FFmpegPCMAudio(source=mp3_path,
+                                            executable=self.ffmpeg_path),
+                     after=lambda e: print('done', e))
 
-        vc.play(discord.FFmpegPCMAudio(source=mp3_path,
-                                       executable=self.ffmpeg_path),
-                after=lambda e: print('done', e))
-
-        while vc.is_playing():
+        while self.vc.is_playing():
             await asyncio.sleep(1)
-        vc.stop()
-
-        await vc.disconnect()
+        self.vc.stop()
 
     def save_to_mp3(self, text, filename):
         """Converts text to speech and saves as mp3"""
